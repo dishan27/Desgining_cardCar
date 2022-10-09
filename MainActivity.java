d0b58cd9e8b4911f94d709e159c74ae104be8590
dishan27 <98529087+dishan27@users.noreply.github.com>
2022-10-09 18:34:50 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,25 @@
+package com.app.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
